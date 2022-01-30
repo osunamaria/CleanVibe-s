995b5fd550a7234b7f2b2df246617d76e2866582
jesusvgessa <jesusvgessa@hotmail.com>
2022-01-30 17:50:33 +0100

    inicio

--- registro/iniciarSesion.php
+++ registro/iniciarSesion.php
@@ -26,4 +26,21 @@
         echo $e;
     }
 
+    $usuario=$_POST["usuario"];
+    $pass= $_POST["contrasena"];
+    //Si el usuario y la contraseña son iguales, inicio sesion
+    if ($usuario == $pass && $usuario != "" && $pass!=""){
+        // Si se usa debe contener sólo caracteres alfanuméricos e ir antes de session_start():
+        session_id("inicioSesion");
+    
+        // Iniciar la sesión
+        session_start();
+    
+        // Variables de sesión:
+        $_SESSION['sesion_iniciada'] = true;
+        $_SESSION['username'] = $usuario;
+        header("location: paginaprincipal.php");
+    }else{
+        header("location: error.php");
+    }//Fin Si
 ?>
