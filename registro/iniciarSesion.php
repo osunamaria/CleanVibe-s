358bf3415f2add708f9e596755ced230e155655e
osunamaria <mariaosuna.20@campuscamara.es>
2022-01-30 18:12:59 +0100

    Cambios en iniciaSesion para coger el id

--- registro/iniciarSesion.php
+++ registro/iniciarSesion.php
@@ -1,29 +1,29 @@
 <?php
 
     $servidor = "localhost";
-    $baseDatos = "clean_vibes";
+    $baseDatos = "cleanvibes";
     $user = "root";
     $pass = "";
 
     $usuario=$_POST["usuario"];
     $contrasena= $_POST["contrasena"];
+
     try {
         $con = new PDO("mysql:host=" . $GLOBALS['servidor'] . ";dbname=" . $GLOBALS['baseDatos'], $GLOBALS['user'], $GLOBALS['pass']);
         $sql = $con->prepare("SELECT id FROM socios WHERE usuario=:usuario AND contrasena=:contrasena");
         $sql->bindParam(":usuario", $usuario);
         $sql->bindParam(":contrasena", $contrasena);
         $sql->execute();
-        $id = "SELECT id FROM socios WHERE usuario=:usuario AND contrasena=:contrasena";
+
+        $id = $sql->fetch(PDO::FETCH_ASSOC); //Recibimos el id
+        if($id==""){
+            echo "Vacio";
+        }else{
+            echo "Lleno";
+        }
+
         $con = null; //Cerramos la conexión
-        echo $id;
-        // if ($id != 0 && $id != null && $id!="") {
-        //     header("Location: ../index.html");
-        //     exit();
-        // } else {
-        //     echo "Datos incorrectos";
-        // }
     } catch (PDOException $e) {
         echo $e;
     }
-
 ?>
