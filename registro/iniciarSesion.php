88f5d82592bc6299e81b3137fe931a743683c319
jesusvgessa <jesusvgessa@hotmail.com>
2022-01-30 17:56:52 +0100

    inicia sesion

--- registro/iniciarSesion.php
+++ registro/iniciarSesion.php
@@ -7,27 +7,26 @@
 
     $usuario=$_POST["usuario"];
     $contrasena= $_POST["contrasena"];
+
     try {
         $con = new PDO("mysql:host=" . $GLOBALS['servidor'] . ";dbname=" . $GLOBALS['baseDatos'], $GLOBALS['user'], $GLOBALS['pass']);
-        $sql = $con->prepare("SELECT id FROM socios WHERE usuario=:usuario AND contrasena=:contrasena");
+        $sql = $con->prepare("SELECT id FROM socios WHERE usuario=:usuario");
         $sql->bindParam(":usuario", $usuario);
-        $sql->bindParam(":contrasena", $contrasena);
         $sql->execute();
-        $id = "SELECT id FROM socios WHERE usuario=:usuario AND contrasena=:contrasena";
-        $con = null; //Cerramos la conexión
+
         echo $id;
-        // if ($id != 0 && $id != null && $id!="") {
-        //     header("Location: ../index.html");
-        //     exit();
-        // } else {
-        //     echo "Datos incorrectos";
-        // }
+        if ($id != 0 && $id != null && $id!="") {
+            header("location: ../index.html");
+            exit();
+        } else {
+            echo "Datos incorrectos";
+        }
+        
+        $con = null; //Cerramos la conexión
     } catch (PDOException $e) {
         echo $e;
     }
 
-    $usuario=$_POST["usuario"];
-    $pass= $_POST["contrasena"];
     //Si el usuario y la contraseña son iguales, inicio sesion
     if ($usuario == $pass && $usuario != "" && $pass!=""){
         // Si se usa debe contener sólo caracteres alfanuméricos e ir antes de session_start():
