7465bec33c42d21a714b787147c5e795000aee66
jesusvgessa <jesusvgessa@hotmail.com>
2022-02-01 18:41:58 +0100

    footer

--- publicaciones/eliminarPublicacion.php
+++ publicaciones/eliminarPublicacion.php
@@ -23,5 +23,20 @@
     <a href="../index.html">[Pagina principal]</a>
 
     <h2><?php echo $error;?></h2>
+    
+    <footer class="d-flex flex-wrap justify-content-center align-items-center py-3 mt-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+            <a href="../index.php" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <img src="../img/logoNaranja.png" alt="logo">
+            </a>
+            <span class="text-muted">&copy; 2021 Company, Inc</span>
+        </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex fs-1">
+            <li class="m-5"><i class="fab fa-instagram"></i></li>
+            <li class="m-5"><i class="fab fa-twitter"></i></li>
+            <li class="m-5"><i class="fab fa-facebook-square"></i></li>
+        </ul>
+    </footer>
 </body>
 </html>
