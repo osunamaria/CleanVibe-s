c7cd5205aa25bac511b08958b4e3fce31a665fb3
osunamaria <mariaosuna.20@campuscamara.es>
2022-02-07 18:25:55 +0100

    Gestion usuarios avances

--- gestion_cuentas/eliminarUsuario.php
+++ gestion_cuentas/eliminarUsuario.php
@@ -1,18 +1,62 @@
-<?php
-    function eliminarUsuario($id){
-        $retorno = false;
-        try{
-            $con = new PDO("mysql:host=" . $GLOBALS['servidor'] . ";dbname=" . $GLOBALS['baseDatos'], $GLOBALS['usuario'], $GLOBALS['pass']);
-            $sql = $con->prepare("DELETE from usuarios where id=:id");
-            $sql->bindParam(":id", $id);
-            $sql->execute();
-            if ($sql->rowCount() > 0){
-                $retorno = true;
-            }
-            $con = null; //Cerramos la conexión
-        }catch(PDOException $e){
-            echo $e;
-        }
-        return $retorno;
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar usuario</title>
+
+    <!-- linkear con fuente belleza -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet">
+
+    <!-- links css -->
+    <link rel="stylesheet" href="../css/headers.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/gestion_cuentas.css">
+
+    <!-- link para iconos -->
+    <link rel="stylesheet" href="../fontawesome-free-5.15.4-web/css/all.min.css">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+</head>
+    <article>  
+    <!-- Preguntar antes de eliminar -->
+    <?php include "operacionesGeneralesUsuarios.php";
+    $id= $_GET["varId"];
+
+    $cumplido=eliminarUsuario($id);
+    $error='Se ha borrado el usuario con el id: ' . $id;
+    if(!$cumplido){
+        $error="Error al borrar el usuario seleccionado";
     }
-?>
+
+    ?>
+
+     <a href="index.php">[Seguir gestionando]</a>
+     <a href="../index.php">[Pagina principal]</a>
+
+     <h2><?php echo $error;?></h2>
+    
+     </article>
+     <footer class="d-flex flex-wrap justify-content-center align-items-center py-3 mt-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+            <a href="../index.php" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <img src="../img/logoNaranja.png" alt="logo">
+            </a>
+            <span class="text-muted">&copy; 2021 Company, Inc</span>
+        </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex fs-1">
+            <li class="m-5"><i class="fab fa-instagram"></i></li>
+            <li class="m-5"><i class="fab fa-twitter"></i></li>
+            <li class="m-5"><i class="fab fa-facebook-square"></i></li>
+        </ul>
+    </footer>
+    <script src="../js/bootstrap.bundle.min"></script>
+</body>
+</html>
